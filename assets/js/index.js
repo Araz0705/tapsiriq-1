8ec376b67b64fd90f53c37edfc0ae3c9c9c5a5d0
namiqabasov <7td508c@code.edu.az>
2023-04-25 16:44:10 +0400

    fayl

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const container2=document.getElementById("container2")
+const mavi=document.getElementById("mavi")
+const qr=document.getElementById("qr")
+mavi.onclick=function () {
+    container2.style.backgroundColor="green"
+}
+qr.onclick=function () {
+    container2.style.backgroundColor="red"
+}
